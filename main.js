84cf5c11250fa391d2f555f47ee638ea3e2d8bad
Abdallah <abdallahchaar90@gmail.comh>
2021-01-08 10:41:55 +0100

    träning om JavaScript function med self invoke

---
+++ main.js
@@ -0,0 +1,24 @@
+/* global console, alert, prompt, myfunc */
+
+
+/*
+function myFunc() {
+
+    "use strict";
+
+    alert("My Function");
+
+}
+myFunc();
+*/
+
+
+(function saywelcome() {
+
+    "use strict";
+
+    alert("Welcome To My Website");
+
+})();
+
+
